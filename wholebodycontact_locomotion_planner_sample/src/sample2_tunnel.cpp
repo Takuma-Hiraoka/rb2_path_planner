9c5d449a0de23815d527145fa7bda3fe845ac0f6
Takuma-Hiraoka <t-hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2024-06-30 17:06:35 +0900

    add tunnel WBLP

--- wholebodycontact_locomotion_planner_sample/src/sample2_tunnel.cpp
+++ wholebodycontact_locomotion_planner_sample/src/sample2_tunnel.cpp
@@ -14,7 +14,9 @@ namespace wholebodycontact_locomotion_planner_sample{
 
     std::shared_ptr<wholebodycontact_locomotion_planner::WBLPParam> param = std::make_shared<wholebodycontact_locomotion_planner::WBLPParam>();
     cnoid::BodyPtr abstractRobot;
-    generateSampleRobot(environment->obstacles, param, abstractRobot);
+    generateSampleRobot(environment->obstacles, param, abstractRobot, true);
+    std::vector<double> initialPose;
+    global_inverse_kinematics_solver::link2Frame(param->variables, initialPose);
 
     // setup viewer
     std::shared_ptr<choreonoid_viewer::Viewer> viewer = std::make_shared<choreonoid_viewer::Viewer>();
@@ -25,6 +27,8 @@ namespace wholebodycontact_locomotion_planner_sample{
 
     viewer->drawObjects();
 
+    param->debugLevel=3;
+    param->viewer = viewer;
     param->gikRootParam.range = 0.5;
     param->gikRootParam.timeout = 30;
     param->gikRootParam.viewer = viewer;
@@ -32,7 +36,7 @@ namespace wholebodycontact_locomotion_planner_sample{
     param->gikRootParam.debugLevel = 0;
     param->gikRootParam.pikParam.debugLevel = 0;
     param->gikRootParam.timeout = 20;
-    param->gikRootParam.threads = 10;
+    param->gikRootParam.threads = 20;
 
     cnoid::Isometry3 goal = param->robot->rootLink()->T();
     goal.translation()[0] += 3.3;
@@ -51,6 +55,29 @@ namespace wholebodycontact_locomotion_planner_sample{
       abstractVariables.push_back(abstractRobot->joint(i));
     }
 
+    for(int i=0;i<path.size();i++){
+      global_inverse_kinematics_solver::frame2Link(path.at(i).first,param->variables);
+      param->robot->calcForwardKinematics(false);
+      param->robot->calcCenterOfMass();
+      global_inverse_kinematics_solver::frame2Link(path.at(i).first,abstractVariables);
+      abstractRobot->calcForwardKinematics(false);
+      abstractRobot->calcCenterOfMass();
+      viewer->drawObjects();
+      std::cerr << "contacts :";
+      for (int j=0; j<path.at(i).second.size(); j++) std::cerr << " " << path.at(i).second[j]->name;
+      std::cerr << std::endl;
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    std::vector<std::pair<std::vector<double>, std::vector<std::shared_ptr<wholebodycontact_locomotion_planner::Contact> > > > contactPath;
+    global_inverse_kinematics_solver::frame2Link(initialPose,param->variables);
+    param->robot->calcForwardKinematics(false);
+    param->robot->calcCenterOfMass();
+    bool solved = wholebodycontact_locomotion_planner::solveWBLP(environment,
+                                                                 param,
+                                                                 path,
+                                                                 contactPath);
+    if (!solved) return;
+
     while(true) {
       // main loop
       for(int i=0;i<path.size();i++){
@@ -61,10 +88,20 @@ namespace wholebodycontact_locomotion_planner_sample{
         abstractRobot->calcForwardKinematics(false);
         abstractRobot->calcCenterOfMass();
         viewer->drawObjects();
-        std::cerr << "contacts :";
-        for (int j=0; j<path.at(i).second.size(); j++) std::cerr << " " << path.at(i).second[j]->link1->name();
-        std::cerr << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      }
+      global_inverse_kinematics_solver::frame2Link(initialPose,param->variables);
+      param->robot->calcForwardKinematics(false);
+      param->robot->calcCenterOfMass();
+      for(int i=0;i<contactPath.size();i++){
+        global_inverse_kinematics_solver::frame2Link(contactPath.at(i).first,param->variables);
+        param->robot->calcForwardKinematics(false);
+        param->robot->calcCenterOfMass();
+        global_inverse_kinematics_solver::frame2Link(contactPath.at(i).first,abstractVariables);
+        abstractRobot->calcForwardKinematics(false);
+        abstractRobot->calcCenterOfMass();
+        viewer->drawObjects();
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
       }
     }
   }
